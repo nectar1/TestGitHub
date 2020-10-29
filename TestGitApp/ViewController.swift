fc08d665309d734fbcdb35ed79dfee1813480507
Dmitry Motyl <dzmitry.matyl@kyriba.com>
2020-10-29 20:05:23 +0300

    Initial commit

---
+++ TestGitApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestGitApp
+//
+//  Created by Dzmitry Matyl on 10/29/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
